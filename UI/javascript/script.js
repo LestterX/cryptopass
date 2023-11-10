0ea9b63e1d27463cb21e0482c5d323c82afe3516
LestterX <lgs.alves@hotmail.com>
2023-11-10 20:50:35 -0300

    feat: remove axios | conf cors | gen cards by db

--- UI/javascript/script.js
+++ UI/javascript/script.js
@@ -1,18 +1,27 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { cardFunctions } from './cardFunctions.js';
+
+const SERVER_PORT = 5550;
 const cardsSection = document.querySelector('.cards');
 
-const card = (name) => cardFunctions.createCard({
-  name: name,
-  password: '123456',
-  assinaturaEletronica: '123514',
-  conta: '7845',
-  cpf: '70937622478',
-  createdAt: '23/05/2014 - 23h45',
-  description: 'Testando apenas isso aqui',
-  email: 'lgs.alves@hotmail.com',
-  updatedAt: '24/05/2014 - 23h45',
-  weblink: 'https://www.minecraft.net/account_settings'
+const data = fetch(`http://localhost:${SERVER_PORT}/password`, {
+  method: 'GET',
+}).then(data => {
+  return data.json();
+}).then(data => {
+  for (let result of data.result) {
+    cardsSection.appendChild(cardFunctions.createCard({
+      name: result.name,
+      password: result.password,
+      assinaturaEletronica: result.assinaturaEletronica,
+      conta: result.conta,
+      cpf: result.cpf,
+      createdAt: result.createdAt,
+      description: result.description,
+      email: result.email,
+      updatedAt: result.updatedAt,
+      weblink: result.weblink
+    }));
+  }
 });
-
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,15 @@
+import 'dotenv/config';
 import express from 'express';
 import { routes } from './routes';
-import 'dotenv/config';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const SERVER_PORT = process.env.SERVER_PORT || 5550;
 const SERVER_PREFIX = process.env.SERVER_PREFIX || '[# Server]';
 
 const server = express();
 
+server.use(cors({origin: [`http://localhost:${SERVER_PORT}`, 'http://127.0.0.1:5500']}));
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 
